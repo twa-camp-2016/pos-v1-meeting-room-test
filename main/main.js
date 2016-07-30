b63532dcd925fb6d9bf050bacd8768094bc7c480
goddessting <1766153116@qq.com>
2016-07-30 16:01:12 +0800

    complete

--- main/main.js
+++ main/main.js
@@ -1,7 +1,84 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+let {loadAllItems, loadPromotions} = require('../spec/fixtures.js');
+let _ = require('lodash');
 
-module.exports = {
-  
+function buildCartItems(tags, allItems) {
+  const cartItems = [];
+
+  for (let tag of tags) {
+    let splittedTag = tag.split('-');
+    let count = parseFloat(splittedTag[1] || 1);
+    let barcode = splittedTag[0];
+
+    let cartItem = cartItems.find(n => n.barcode === barcode);
+    if (cartItem) {
+      cartItem.count += count;
+    } else {
+      let item = allItems.find(n => n.barcode === barcode);
+      cartItems.push({item: item, count: count});
+    }
+
+    return cartItems;
+  }
 }
+
+function buildReceiptItems(cartItems, promotions) {
+  const receiptItems = [];
+
+  for (let cartItem of cartItems) {
+    const subtotal = cartItem.count * cartItem.item.price;
+    let barcode = promotions[0].barcodes.find(n => n === cartItem.item.barcode);
+    if (barcode) {
+      
+      barcode.count >= 2 ? receiptItems.push({
+        cartItem: cartItem,
+        subtotal: subtotal, saved: cartItem.item.price
+      }) : receiptItems.push({cartItem: cartItem, subtotal: subtotal, saved: 0});
+    }else {
+    receiptItems.push({cartItem: cartItem, subtotal: subtotal, saved: 0});
+    }
+  }
+
+  return receiptItems;
+}
+
+function buildReceipt(receiptItems) {
+  let total = 0;
+  let totalSaved = 0;
+  for (let receiptItem of receiptItems) {
+    total += receiptItem.subtotal;
+    totalSaved += receiptItem.saved;
+  }
+  return {receiptItem: receiptItems, total: total ,totalSaved: totalSaved};
+}
+
+function buildReceiptText(receipt){
+  let expectText = '***<没钱赚商店>收据***\n';
+
+  for(let receiptItem of receipt.receiptItem){
+    expectText += `名称:${receiptItem.cartItem.item.name}，\
+数量：${receiptItem.cartItem.count}${receiptItem.cartItem.item.unit}，\
+单价：${receiptItem.cartItem.item.price}(元)，\
+小计：${receiptItem.subtotal}(元)
+`}
+  expectText +='----------------------';
+  expectText +=`总计：${receipt.total}(元)
+节省：${receipt.totalSaved}(元)
+**********************`;
+  console.log(expectText);
+}
+
+function printReceipt(input) {
+  let allItems = loadAllItems();
+
+  let cartItems = buildCartItems(input, allItems);
+
+  let promotions = loadPromotions();
+  let receiptItems = buildReceiptItems(cartItems, promotions);
+
+  let receipt = buildReceipt(receiptItems);
+  buildReceiptText(receipt);
+}
+
+module.exports = {printReceipt, buildCartItems, buildReceiptItems, buildReceipt};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,7 +1,95 @@
 'use strict';
-
+let {printReceipt,buildCartItems, buildReceiptItems,buildReceipt} = require('../main/main.js');
 describe('pos', () => {
 
+  it('统计商品信息', () => {
+    const input = ['ITEM000005-2'];
+    const allItems = [{
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    },
+      {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50
+      }];
+    const expectResult = buildCartItems(input, allItems);
+    const result = [{
+      item: {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50
+      }, count: 2
+    }];
+
+    expect(expectResult).toEqual(result);
+  });
+
+  it('统计小计', () => {
+    const promotions = [
+      {
+        type: 'BUY_TWO_GET_ONE_FREE',
+        barcodes: [
+          'ITEM000000',
+          'ITEM000001',
+          'ITEM000005'
+        ]
+      }
+    ];
+    const input = [{
+      item: {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50
+      }, count: 2
+    }];
+    const result = [{
+      cartItem: {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        }, count: 2
+      }, subtotal: 9,
+        saved:0
+    }];
+    const expectResult = buildReceiptItems(input, promotions);
+
+    expect(expectResult).toEqual(result);
+  });
+  it('统计小计', () => {
+
+    const input =  [{
+      cartItem: {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        }, count: 2
+      }, subtotal: 9,
+      saved:4.50
+    }];
+    const result = {receiptItem: [{
+      cartItem: {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        }, count: 2
+      }, subtotal: 9,saved:4.50
+    }],total:9,totalSaved:4.50};
+    const expectResult = buildReceipt(input);
+
+    expect(expectResult).toEqual(result);
+  });
   it('should print text', () => {
 
     const tags = [
