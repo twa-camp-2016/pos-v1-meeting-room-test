a071fd5a61be32df0e61870eb7d3fa1ccffe3e50
Aym-fuhong <18829287015@163.com>
2016-07-30 17:02:30 +0800

    作业

--- main/main.js
+++ main/main.js
@@ -1,7 +1,103 @@
 'use strict';
+let _ = require('lodash')
+function formatTags(tags){
+  return tags.map((tag)=>{
+    if(tag.includes('-')){
+      let [barcode,count] = tag.split('-');
+      return {barcode,count:parseFloat(count)}
+    }else{
+      return {barcode:tag, count:1}
+    }
+  });
+}
+function _getExistentElementByBarcode(array,barcode){
+  return array.find( arr =>  arr.barcode === barcode );
+}
+function countTags(formattedTags){
+ return formattedTags.reduce((result,tag)=>{
+  let element = _getExistentElementByBarcode(result,tag.barcode);
+  if(element){
+    element.count += tag .count;
+  }else{
+    result.push({barcode:tag.barcode,count:tag.count});
+  }
+  return result;
+},[])
+}
+function getCartItems(countedBarcodes,allItems){
+  return countedBarcodes.map(({barcode,count})=>{
+    let {name,unit,price} = _getExistentElementByBarcode(allItems, barcode);
+    return {barcode,name,unit,price,count};
+  });
+}
+function _fixPrice(number){
+  return parseFloat(number.toFixed(2))
+}
+function getPromotionItems(cartItems,promotions){
+  let currentPromotion = promotions.find((promotion)=>promotion.type="BUY_TWO_GET_ONE_FREE")
+  return cartItems.map((cartItem)=>{
+    let haspromoted = currentPromotion.barcodes.includes(cartItem.barcode)&&cartItem.count>2;
+    let totalprice = cartItem.price*cartItem.count;
+    let saved = haspromoted?cartItem.price * cartItem.count/3:0;
+    let payprice = totalprice-saved;
+    return Object.assign({},cartItem,{
+      payprice,
+      saved:_fixPrice(saved)
+    })
+  })
+}
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function getTotalprice(promotedItems){
+  return promotedItems.reduce((result,{payprice,saved})=>{
+      result.totalpayprice += payprice;
+      result.totalsaved += saved;
+      return result;
+    },
+    {totalpayprice:0,totalsaved:0})
+}
+function getReceipt(promotedItems, totalPrices){
+  return {
+    receiptItems:promotedItems.map(({name,unit,price,count,payprice,saved})=>{
+      return {name,unit,price,count,payprice,saved}
+    }),
+    totalpayprice:totalPrices.totalpayprice,
+    totalsaved:totalPrices.totalsaved
+  }
+}
+function getReceiptString(receipt){
+  let totalprice = receipt.totalpayprice;
+  let saved = receipt.totalsaved;
+  let receiptString = "";
+  for(let receiptItem of receipt.receiptItems){
+    receiptString += `名称:${receiptItem.name},数量:${receiptItem.count},单价:${receiptItem.price.toFixed(2)}(元),小计:${receiptItem.payprice.toFixed(2)}(元)`;
+    receiptString += "\n";
+  }
+  const result = `***<没钱赚商店>收据***
+${receiptString}----------------------
+总计:${totalprice.toFixed(2)}(元)
+节省:${saved.toFixed(2)}(元)
+**********************`;
+  return result;
+}
+function printReceipt(tags){
+  let allItems = loadAllItems();
+  let formattedTags = formatTags(tags);
+  let countedBarcodes = countTags(formattedTags);
+  let cartItems = getCartItems(countedBarcodes,allItems);
+  let promotions = loadPromotions();
+  let promotionItems = getPromotionItems(cartItems,promotions);
+  let totalprice = getTotalprice(promotionItems);
+  let receiptItems = getReceipt(promotionItems,totalprice);
+  let receiptString = printReceipt(receiptItems);
+  console.log(receiptString);
 
+}
 module.exports = {
-  
+  formatTags:formatTags,
+  countTags:countTags,
+  getCartItems:getCartItems,
+  getPromotionItems:getPromotionItems,
+  getTotalprice:getTotalprice,
+  getReceipt:getReceipt,
+  getReceiptString:getReceiptString
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,33 +1,164 @@
 'use strict';
+let {formatTags,countTags,getCartItems,getPromotionItems,getTotalprice,getReceipt,getReceiptString} = require('../main/main.js')
 
 describe('pos', () => {
 
-  it('should print text', () => {
-
-    const tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2.5',
-      'ITEM000005',
-      'ITEM000005-2',
+  it('should get formatted tags',() => {
+    let tags = ['ITEM000001', 'ITEM000002-3'];
+    let formattedTags = formatTags(tags);
+    let expected = [
+      {barcode: 'ITEM000001', count: 1},
+      {barcode: 'ITEM000002', count: 3}
     ];
-
-    spyOn(console, 'log');
-
-    printReceipt(tags);
-
-    const expectText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：58.50(元)
-节省：7.50(元)
-**********************`;
-
-    expect(console.log).toHaveBeenCalledWith(expectText);
+    expect(formattedTags).toEqual(expected);
   });
+  it('should get countTags',() =>{
+    let input = [{barcode: 'ITEM000001', count: 1}, {barcode: 'ITEM000002', count: 3}, {barcode: 'ITEM000001', count: 3}];
+    let output = countTags(input);
+    let expected = [{barcode: 'ITEM000001', count: 4}, {barcode: 'ITEM000002', count: 3}];
+    expect(output).toEqual(expected);
+  })
+  it('should get cartTags',() =>{
+    let allItems = loadAllItems();
+    let countedBarcodes = [{barcode: 'ITEM000000', count: 2}, {barcode: 'ITEM000001', count: 3}];
+    let cartItems = getCartItems(countedBarcodes, loadAllItems());
+    let expected = [{barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 2},
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 3}];
+    expect(cartItems).toEqual(expected);
+  });
+  it('should get promotionItems',() =>{
+   let input = [{barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 2},
+     {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 3}];
+   let output = getPromotionItems(input,loadPromotions());
+    let expected = [{barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 2,payprice:6.00,saved:0},
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 3,payprice:6.00,saved:3}];
+  })
+  it('should get getTotalprice',() => {
+    let input = [{barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 2,payprice:6.00,saved:0},
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 3,payprice:6.00,saved:3}];
+    let output = getTotalprice(input);
+    let expected = {
+      totalpayprice: 12.00,
+      totalsaved: 3
+    };
+    expect(output).toEqual(expected);
+  });
+  it('should get receipt',()=>{
+    let input1 = [{barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 2,payprice:6.00,saved:0},
+      {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 3,payprice:6.00,saved:3}];
+      let input2 =  {
+        totalpayprice: 12.00,
+        totalsaved: 3
+      };
+    let output = getReceipt(input1,input2);
+    let expected = {
+      receiptItems: [
+        { name: '可口可乐', unit: '瓶', price: 3.00, count: 2,payprice:6.00,saved:0},
+        { name: '雪碧', unit: '瓶', price: 3.00, count: 3,payprice:6.00,saved:3}
+      ],
+      totalpayprice: 12.00,
+      totalsaved: 3
+    };
+    expect(output).toEqual(expected);
+  })
+  it('should get receiptString',() =>{
+    let input = {
+      promotedItems: [
+        { name: '可口可乐', unit: '瓶', price: 3.00, count: 2,payprice:6.00,saved:0},
+        { name: '雪碧', unit: '瓶', price: 3.00, count: 3,payprice:6.00,saved:3}
+      ],
+      totalpayprice: 12.00,
+      totalsaved: 3
+    };
+    let output = getReceiptString(input);
+    let expected = `***<没钱赚商店>收据***
+名称：可口可乐，数量：2瓶，单价：3.00(元)，小计：6.00(元)
+名称：雪碧，数量：3瓶，单价：3.00(元)，小计：6.00(元)
+----------------------
+总计：12.00(元)
+节省：3.00(元)
+**********************`;
+    expect(output).toEqual(expected);
+  })
+//  it('should print text', () => {
+//
+//    const tags = [
+//      'ITEM000001',
+//      'ITEM000001',
+//      'ITEM000001',
+//      'ITEM000001',
+//      'ITEM000001',
+//      'ITEM000003-2.5',
+//      'ITEM000005',
+//      'ITEM000005-2',
+//    ];
+//
+//    spyOn(console, 'log');
+//
+//    printReceipt(tags);
+//
+//    const expectText = `***<没钱赚商店>收据***
+//名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+//名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+//名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+//----------------------
+//总计：58.50(元)
+//节省：7.50(元)
+//**********************`;
+//
+//    expect(console.log).toHaveBeenCalledWith(expectText);
+//  });
 });
+function loadAllItems() {
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: '苹果',
+      unit: '斤',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: '荔枝',
+      unit: '斤',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: '方便面',
+      unit: '袋',
+      price: 4.50
+    }
+  ];
+}
+
+function loadPromotions() {
+  return [
+    {
+      type: 'BUY_TWO_GET_ONE_FREE',
+      barcodes: [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+      ]
+    }
+  ];
+}
