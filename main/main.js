4fe4e53995f49ce3af025f5dd035f81fae0f54f5
fanfanqu <18829291202@163.com>
2016-07-30 14:46:47 +0800

    getFormattedTags

--- main/main.js
+++ main/main.js
@@ -1,7 +1,23 @@
 'use strict';
-
-//TODO: 请在该文件中实现练习要求并删除此注释
-
-module.exports = {
-  
+let tags = ['ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000003-2']
+function getFormattedTags(tags) {
+  let result = tags.map(tag=> {
+    if (tag.includes('-')) {
+      let [barcode,count]=tag.split('-');
+      return {
+        barcode: barcode, count: parseFloat(count)
+      }
+    } else {
+      return {
+        barcode: tag, count: 1
+      }
+    }
+  });
+  return result;
 }
+function printReceipt(tags) {
+  let formattedTags = getFormattedTags(tags);
+  return formattedTags;
+}
+console.log(printReceipt(tags));
+module.exports = {getFormattedTags}
