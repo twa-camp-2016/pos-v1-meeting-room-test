e7db93ce37676a014a20908a9da438eb6b26238e
baixue1016 <1773158403@qq.com>
2016-07-30 15:39:49 +0800

    three

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
 'use strict';
+let _ = require('lodash');
 let tags = [
   'ITEM000001',
   'ITEM000001',
@@ -9,7 +10,46 @@ let tags = [
   'ITEM000005',
   'ITEM000005-2',
 ];
-let _ = require('lodash');
+function loadAllItems() {
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: '苹果',
+      unit: '斤',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: '荔枝',
+      unit: '斤',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: '方便面',
+      unit: '袋',
+      price: 4.50
+    }
+  ];
+}
 function getFormattedTags(tags) {
   return _.chain(tags).map(x=> {
     if (_.includes(x, '-')) {
@@ -48,10 +88,31 @@ function getCountBarcodes(formattedTags) {
   });
   return result;
 }
+
+function buildCartItems(countedBarcodes, allItems) {
+  let result = [];
+  _.map(countedBarcodes, x=> {
+    let item = getExistByBarcode(allItems, x.barcode);
+    let cartItem = {
+      barcode: item.barcode,
+      name: item.name,
+      unit: item.unit,
+      price: item.price,
+      count: x.count
+    };
+    result.push(cartItem);
+  });
+  return result;
+}
+
 let formattedTags = getFormattedTags(tags);
-getCountBarcodes(formattedTags);
+let countedBarcodes = getCountBarcodes(formattedTags);
+let allItems = loadAllItems();
+buildCartItems(countedBarcodes, allItems);
 
 module.exports = {
   getFormattedTags: getFormattedTags,
-  getCountBarcodes: getCountBarcodes
-}
+  getCountBarcodes: getCountBarcodes,
+  buildCartItems: buildCartItems,
+  loadAllItems: loadAllItems
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,6 +1,6 @@
 'use strict';
-let {getFormattedTags, getCountBarcodes}=require('../main/main.js');
-
+let {getFormattedTags, getCountBarcodes, buildCartItems, loadAllItems}=require('../main/main.js');
+//let {loadAllItems}=require('../spec/fixtures.js');
 describe('pos', () => {
   fit('should get formatted tags', () => {
     const tags = [
@@ -28,7 +28,7 @@ describe('pos', () => {
     expect(result).toEqual(expected);
   });
 
-  fit("should print countedBarcodes", () => {
+  fit("should countedBarcodes", () => {
     const formattedTags = [
       { barcode: 'ITEM000001', count: 1 },
       { barcode: 'ITEM000001', count: 1 },
@@ -45,6 +45,23 @@ describe('pos', () => {
       {barcode: 'ITEM000001', count: 5},
       {barcode: 'ITEM000003', count: 2.5},
       {barcode: 'ITEM000005', count: 3}];
+
+    expect(result).toEqual(expected);
+  });
+
+  fit("should buildCartItems", () => {
+    const countedBarcodes = [
+      {barcode: 'ITEM000001', count: 5},
+      {barcode: 'ITEM000003', count: 2.5},
+      {barcode: 'ITEM000005', count: 3}];
+    const allItems = loadAllItems();
+
+    let result = buildCartItems(countedBarcodes, allItems);
+
+    const expected = [
+      { barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3, count: 5 },
+      { barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15, count: 2.5 },
+      { barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.5, count: 3 } ];
 
     expect(result).toEqual(expected);
   });
