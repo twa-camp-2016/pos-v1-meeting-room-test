97c1f7d2ec85c9a71bb7444cd819758423b2856d
duancoin <duanjm104@163.com>
2016-07-30 17:01:47 +0800

    段锦梦

--- main/main.js
+++ main/main.js
@@ -51,7 +51,79 @@ function buildItems(countedItems, allItems) {
       };
     })
     .value();
+}
 
+function promotionItems(buildedItems, allPromotinos) {
+  return _.chain(buildedItems)
+    .map(buildedItem =>{
+      let promotion = allPromotinos.find(n => n.type === 'BUY_TWO_GET_ONE_FREE');
+      let temp = promotion.barcodes.find(n => n === buildedItem.barcode);
+      let totalSaved = 0;
+
+      if(temp){
+        let savedCount = _.floor(buildedItem.count / 3);
+        totalSaved = parseFloat((savedCount * buildedItem.price).toFixed(2));
+      }
+
+      let totalPayPrice = parseFloat((buildedItem.count * buildedItem.price - totalSaved).toFixed(2));
+      return {
+        barcode: buildedItem.barcode,
+        name: buildedItem.name,
+        unit: buildedItem.unit,
+        price: buildedItem.price,
+        count: buildedItem.count,
+        totalSaved,
+        totalPayPrice
+      }
+    })
+    .value();
+}
+
+function buildReceipt(promotedItems) {
+  let totalPayPrices = 0;
+  let totalSaveds = 0;
+
+  promotedItems.map(promotedItem =>{
+    totalPayPrices += promotedItem.totalPayPrice;
+    totalSaveds += promotedItem.totalSaved;
+  });
+
+  return {
+    promotedItems,
+    totalPayPrices,
+    totalSaveds
+  };
+}
+// ***<没钱赚商店>收据***
+// 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+// 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+// 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+// ----------------------
+// 总计：58.50(元)
+// 节省：7.50(元)
+// **********************
+function buildReceiptString(receipt) {
+  let lines = [];
+  lines.push('***<没钱赚商店>收据***');
+  receipt.promotedItems.map(item =>{
+    lines.push(`名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price}(元)，小计：${item.totalPayPrice}(元)`);
+  });
+  lines.push('----------------------');
+  lines.push(`总计：${receipt.totalPayPrices}(元)`);
+  lines.push(`节省：${receipt.totalSaveds}(元)`);
+  lines.push('**********************');
+
+  return lines.join('\n');
+}
+
+function printReceipt(tags) {
+  let formattedTags = formatTags(tags);
+  let countedItems = countItmes(formattedTags);
+  let buildedItems = buildItems(countedItems, loadAllItems());
+  let promotedItems = promotionItems(buildedItems, loadPromotions());
+  let receipt = buildReceipt(promotedItems);
+
+  console.log(buildReceiptString(receipt));
 }
 
 function loadAllItems() {
@@ -95,10 +167,28 @@ function loadAllItems() {
   ];
 }
 
+function loadPromotions() {
+  return [
+    {
+      type: 'BUY_TWO_GET_ONE_FREE',
+      barcodes: [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+      ]
+    }
+  ];
+}
+
+
 module.exports = {
   formatTags: formatTags,
   countItmes: countItmes,
   buildItems: buildItems,
   loadAllItems: loadAllItems,
+  loadPromotions: loadPromotions,
+  promotionItems: promotionItems,
+  buildReceipt: buildReceipt,
+  printReceipt: printReceipt
 }
 
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -6,6 +6,10 @@ let {
   countItmes,
   buildItems,
   loadAllItems,
+  loadPromotions,
+  promotionItems,
+  buildReceipt,
+  printReceipt
 }
   = require('../main/main');
 
@@ -168,32 +172,166 @@ describe('pos', () => {
     expect(buildedItems).toEqual(expected)
   });
 
-//   it('should print text', () => {
-//
-//     const tags = [
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000003-2.5',
-//       'ITEM000005',
-//       'ITEM000005-2',
-//     ];
-//
-//     spyOn(console, 'log');
-//
-//     printReceipt(tags);
-//
-//     const expectText = `***<没钱赚商店>收据***
-// 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-// 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-// 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-// ----------------------
-// 总计：58.50(元)
-// 节省：7.50(元)
-// **********************`;
-//
-//     expect(console.log).toHaveBeenCalledWith(expectText);
-//   });
+  it('should promotionItems', () =>{
+    const buildedItems = [
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 5
+      },
+      {
+        barcode: 'ITEM000003',
+        name: '荔枝',
+        unit: '斤',
+        price: 15.00,
+        count: 2.5
+      },
+      {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50,
+        count: 3
+      }
+    ];
+
+    let allPromotions = loadPromotions();
+
+    let promotedItems = promotionItems(buildedItems, allPromotions);
+
+    const expected = [
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 5,
+        totalSaved: 3.00,
+        totalPayPrice: 12.00
+      },
+      {
+        barcode: 'ITEM000003',
+        name: '荔枝',
+        unit: '斤',
+        price: 15.00,
+        count: 2.5,
+        totalSaved: 0,
+        totalPayPrice: 37.50
+      },
+      {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50,
+        count: 3,
+        totalSaved: 4.50,
+        totalPayPrice: 9.00
+      }
+    ];
+
+    expect(promotedItems).toEqual(expected);
   });
+
+  it('should buildReceipt', ()=>{
+    const promotedItems = [
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 5,
+        totalSaved: 3.00,
+        totalPayPrice: 12.00
+      },
+      {
+        barcode: 'ITEM000003',
+        name: '荔枝',
+        unit: '斤',
+        price: 15.00,
+        count: 2.5,
+        totalSaved: 0,
+        totalPayPrice: 37.50
+      },
+      {
+        barcode: 'ITEM000005',
+        name: '方便面',
+        unit: '袋',
+        price: 4.50,
+        count: 3,
+        totalSaved: 4.50,
+        totalPayPrice: 9.00
+      }
+    ];
+
+    let receipt = buildReceipt(promotedItems);
+
+    const expected = {
+      promotedItems: [
+        {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00,
+          count: 5,
+          totalSaved: 3.00,
+          totalPayPrice: 12.00
+        },
+        {
+          barcode: 'ITEM000003',
+          name: '荔枝',
+          unit: '斤',
+          price: 15.00,
+          count: 2.5,
+          totalSaved: 0,
+          totalPayPrice: 37.50
+        },
+        {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50,
+          count: 3,
+          totalSaved: 4.50,
+          totalPayPrice: 9.00
+        }
+      ],
+      totalPayPrices: 58.50,
+      totalSaveds: 7.50
+    };
+
+    expect(receipt).toEqual(expected);
+  });
+
+  it('should print text', () => {
+
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+
+    spyOn(console, 'log');
+
+    printReceipt(tags);
+
+    const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+  });
+  });
+
+
