f4709f67a6c8ed1389441227125c40a530b4f173
zhagnian <zhagnian@baidu.com>
2016-08-01 20:36:19 +0800

    zhangqians

--- main/main.js
+++ main/main.js
@@ -2,6 +2,7 @@
 
 //TODO: 请在该文件中实现练习要求并删除此注释
 let _ = require('lodash');
+let {loadAllItems,loadPromotions}=require('../spec/fixtures.js');
 let tags = [
   'ITEM000001',
   'ITEM000001',
@@ -45,126 +46,91 @@ function getCartCounts(formattedCounts) {
   return b;
 //console.log(b);
 }
-function loadAllItems() {
-  return [
-    {
-      barcode: 'ITEM000001',
-      name: '雪碧',
-      unit: '瓶',
-      price: 3.00
-    },
-    {
-      barcode: 'ITEM000003',
-      name: '荔枝',
-      unit: '斤',
-      price: 15.00
-    },
-    {
-      barcode: 'ITEM000005',
-      name: '方便面',
-      unit: '袋',
-      price: 4.50
-    }];
-}
-function buildAllItems(cartCount,allItems){
-  let a=_.chain(cartCount)
-    .map(x=>{
-      let found=_getElementById(allItems, x.barcode)
-      if(found)
-      {
-        return {barcode:x.barcode,name:found.name,count:x.count,unit:found.unit,price:found.price,}
+
+function buildAllItems(cartCount, allItems) {
+  let a = _.chain(cartCount)
+    .map(x=> {
+      let found = _getElementById(allItems, x.barcode)
+      if (found) {
+        return {barcode: x.barcode, name: found.name, count: x.count, unit: found.unit, price: found.price,}
       }
     })
     .value();
-return a;
+  return a;
   //console.log(a);
 }
-function promotionItems(){
-  return [
-    {
-      type: 'BUY_TWO_GET_ONE_FREE',
-      barcodes: [
-        'ITEM000000',
-        'ITEM000001'
-      ]
-    },
-    {
-      type: 'OTHER_PROMOTION',
-      barcodes: [
-        'ITEM000003',
-        'ITEM000004'
-      ]
-    }
-  ];
-}
 
-
-function builtPromotedItems(builtedCartItems,promottedItems){
-  let currentPromotion=_.find(promottedItems,(promotion)=>promotion.type==='BUY_TWO_GET_ONE_FREE');
-  let a= _.map(builtedCartItems,(cartItems)=> {
+function builtPromotedItems(builtedCartItems, promottedItems) {
+  let currentPromotion = _.find(promottedItems, (promotion)=>promotion.type === 'BUY_TWO_GET_ONE_FREE');
+  let a = _.map(builtedCartItems, (cartItems)=> {
     let hasPromoted = currentPromotion.barcodes.includes(cartItems.barcode);
     let saveCount = Math.floor(cartItems.count / 3);
     //let savePrice=saveCount*cartItems.price;
     let totalPrice = cartItems.count * cartItems.price;
     let savePrice = hasPromoted ? cartItems.price * saveCount : 0;
     let payPrice = totalPrice - savePrice;
-    //console.log(_fixPrice(savePrice));
-    // console.log(payPrice);
-    //console.log(Object.assign({}, cartItems, {payPrices: payPrice, savePrices: savePrice}));
     return _.assign({}, cartItems, {payPrices: payPrice, savePrices: savePrice});
   });
-  let result=[];
-  for(let element of a)
-  {
+  let result = [];
+  for (let element of a) {
     result.push(element);
   }
-return result;
+  return result;
 //console.log(result);
 }
 
-function calculateTotalPrice(builtedPromtedItems){
-  let a= _.reduce(builtedPromtedItems,(result,{payPrices,savePrices})=>
-    {
-      result.totalPayPrice+=payPrices;
-      result.totalSavePrice+=savePrices;
+function calculateTotalPrice(builtedPromtedItems) {
+  let a = _.reduce(builtedPromtedItems, (result, {payPrices, savePrices})=> {
+      result.totalPayPrice += payPrices;
+      result.totalSavePrice += savePrices;
       return result;
-    },{totalPayPrice:0,totalSavePrice:0}
+    }, {totalPayPrice: 0, totalSavePrice: 0}
   );
   //console.log(a);
   return a;
 }
-function builtReceipt(builtedPromtedItems,calculateItems) {
-  let a= {
-    promotedItems: _.map(builtedPromtedItems,({name, unit, price, count, payPrices, savePrices})=> {
+function builtReceipt(builtedPromtedItems, calculateItems) {
+  let a = {
+    promotedItems: _.map(builtedPromtedItems, ({name, unit, price, count, payPrices, savePrices})=> {
       return {name, unit, price, count, payPrices, savePrices};
     }),
-    totalPayPrice:calculateItems.totalPayPrice,
-    totalSavePrice:calculateItems.totalSavePrice
+    totalPayPrice: calculateItems.totalPayPrice,
+    totalSavePrice: calculateItems.totalSavePrice
   };
   //console.log(a);
   return a;
 }
 
-function builtNotes(buildReceipt){
-  let lines=['***<没钱赚商店>收据***'];
-  for(let item of buildReceipt.promotedItems)
-  {
-    lines.push(`名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price}(元)小计：${item.payPrices}`)
-  }
+function builtNotes(buildReceipt) {
+  let lines = ['***<没钱赚商店>收据***'];
+
+
+   for(let item of buildReceipt.promotedItems) {
+     lines.push(`名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${item.payPrices.toFixed(2)}(元)`);
+   }
+  lines.push('----------------------');
+  lines.push(`总计：${buildReceipt.totalPayPrice.toFixed(2)}(元)`);
+  lines.push(`节省：${buildReceipt.totalSavePrice.toFixed(2)}(元)`);
+  lines.push('**********************');
+let notes=lines.join('\n');
+  require(`fs`).writeFileSync('1.txt',notes);
+  return notes;
 
 }
-
 
 
 function Receipt(tags) {
   let formattedCounts = formatCartCount(tags);
   let cartCount = getCartCounts(formattedCounts);
-  let allItems=loadAllItems();
-  let builtedCartItems=buildAllItems(cartCount,allItems);
-  let promottedItems=promotionItems();
-  let builtedPromtedItems=builtPromotedItems(builtedCartItems,promottedItems);
-  let calculateItems=calculateTotalPrice(builtedPromtedItems);
-  let buildReceipt=builtReceipt(builtedPromtedItems,calculateItems)
+  let allItems = loadAllItems();
+  let builtedCartItems = buildAllItems(cartCount, allItems);
+  let promottedItems = loadPromotions();
+  let builtedPromtedItems = builtPromotedItems(builtedCartItems, promottedItems);
+  let calculateItems = calculateTotalPrice(builtedPromtedItems);
+  let buildReceipt = builtReceipt(builtedPromtedItems, calculateItems);
+  let notes=builtNotes(buildReceipt);
+  console.log(notes);
+  return notes;
 }
 
 Receipt(tags);
@@ -172,12 +138,14 @@ Receipt(tags);
 module.exports = {
   formatCartCount: formatCartCount,
   getCartCounts: getCartCounts,
-  loadAllItems:loadAllItems,
-  buildAllItems:buildAllItems,
-  promotionItems:promotionItems,
-  builtPromotedItems:builtPromotedItems,
-  calculateTotalPrice:calculateTotalPrice,
-  builtReceipt:builtReceipt
+  loadAllItems: loadAllItems,
+  buildAllItems: buildAllItems,
+  loadPromotions: loadPromotions,
+  builtPromotedItems: builtPromotedItems,
+  calculateTotalPrice: calculateTotalPrice,
+  builtReceipt: builtReceipt,
+  builtNotes:builtNotes,
+  Receipt:Receipt
 };
 
 
